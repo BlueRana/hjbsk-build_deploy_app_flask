eb966969308b970f458645bc9dd07dd50390e813
Sarah <bluefrog79sl@gmail.com>
2025-08-28 17:09:47 -0500

    Create mathematics.py

---
+++ Maths/mathematics.py
@@ -0,0 +1,15 @@
+
+# summation function with two values
+def summation(a,b):
+   result = a + b
+   return result
+
+# multiplication function with two values
+def multiplication(a,b):
+   result = a * b
+   return result
+
+# subtraction function with two values
+def subtraction(a,b):
+   result = a - b
+   return result
